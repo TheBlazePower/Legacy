ba7feedf8292e09a0245a443ec0bb9105971ec4e
TheBlazePower <38249663+TheBlazePower@users.noreply.github.com>
2018-05-09 08:11:37 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const express = require("express");
 const bot = new Discord.Client({disableEveryone: true});
+const prefix = `.`
 bot.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
@@ -101,9 +101,8 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
   let sender = message.author;
-  if (!message.content.startsWith(botconfig.prefix)) return;
+  if (!message.content.startsWith(prefix)) return;
 
-  let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);

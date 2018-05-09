f3aa4d7dc194aca8b9c2f83967d4a138b58feef9
TheBlazePower <38249663+TheBlazePower@users.noreply.github.com>
2018-05-09 08:24:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-const express = require("express");
 const bot = new Discord.Client({disableEveryone: true});
 const prefix = `.`
 bot.commands = new Discord.Collection();

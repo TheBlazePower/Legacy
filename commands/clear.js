e182d5aae69a6719c9b58fc8abd9545798719efc
TheBlazePower <38249663+TheBlazePower@users.noreply.github.com>
2018-05-10 12:35:41 +0700

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
   if(!args[0]) return message.channel.send("no");

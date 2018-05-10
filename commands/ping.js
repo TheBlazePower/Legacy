dcde0e597239d9378d11c5321cdd977df0511546
TheBlazePower <38249663+TheBlazePower@users.noreply.github.com>
2018-05-10 12:37:01 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
     message.delete()
     let then = Date.now()
     let embed = new Discord.RichEmbed()

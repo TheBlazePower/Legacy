4f5ffbf1c1139cb1b634229e2cdb59051c3ef679
TheBlazePower <38249663+TheBlazePower@users.noreply.github.com>
2018-05-10 12:36:26 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
     message.delete()
     let botembed = new Discord.RichEmbed()
     .setTitle(`${bot.user.username}, Command List`)
